442a83a120e31f2ed22a6b6d16b56b3953359154
leehyeon2021 <leehyeon2021@gmail.com>
2025-12-28 22:17:10 +0900

    251228 복습

--- day003/practiceJS3.js
+++ day003/practiceJS3.js
@@ -23,4 +23,42 @@
     // // 2. 2025년 기준 현재 나이 계산, 콘솔에 출력
     // console.log(`당신은 ${2025 - born}세 입니다.`)
 
-// 문제4: 
+// 문제4: BMI 지수 계산
+    // // 1. 키와 몸무게 입력 받기
+    // let cm = Number(prompt(`키(cm)를 입력하시오. (숫자만)`));
+    // let kg = Number(prompt(`몸무게(kg)를 입력하시오. (숫자만)`));
+    // // 2. BMI 지수 계산 ( 몸무게(kg) / 키(m) * 키(m) )
+    // let BMI = kg / (cm/100)*(cm/100);
+    // // 3. 콘솔 입력
+    // console.log(`BMI 지수: ${BMI}`);
+
+// 문제5: 관리자 확인
+    // // 1. 아이디 이메일 입력 받기
+    // let id = prompt(`아이디를 입력하시오.`);
+    // let email = prompt(`이메일을 입력하시오`);
+    // // 2. 아이디 이메일 확인
+    // let manager = id==`admin` && email==`admin@test.com`;
+    // // 3. 여부에 따라 관리자 / 일반 사용자 입력
+    // let result = manager ? `관리자`:`일반 사용자`;
+    // console.log(result);
+
+// 문제6: 학점 변환기 (HTML 출력)
+    // // 1. 100점 만점 점수 입력 받기
+    // let int1 = Number(prompt(`점수를 입력하시오. (숫자만 입력)`));
+    // // 2. 등급 부여
+    // let result1 = int1 >= 90 ? `A` : int1 >= 80 && int1 <= 90 ? `B` : `C`;
+    // // 3. HTMl 문서에 <h2> 태그로 출력
+    // console.log(`등급: ${result1}`);
+    // document.write(`<h2>등급: ${result1}</h2>`);
+
+// 문제7: 청년 이벤트 대상 확인
+    // // 1. 나이 입력 받기
+    // let age = Number(prompt(`나이를 입력하시오. (숫자만)`));
+    // // 2. 20대 해당 여부에 따라 대사 출력
+    // let result2 = age >= 20 && age <= 29 ? `이벤트 대상입니다.` : `이벤트 대상이 아닙니다.`;
+    // console.log( result2 );
+
+
+// 문제8: '좋아요' 카운터 만들기
+    
+    // 모르겠어!
