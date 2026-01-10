b57f67c8ec59d778b1defd1bd605904b4f38ae73
leehyeon2021 <leehyeon2021@gmail.com>
2026-01-10 21:11:54 +0900

    260110 연습

--- day011/practice6.js
+++ day011/practice6.js
@@ -1,4 +1,4 @@
-// '지역변수': 그 지역에서만 사용할 수 있는 변수
+// '지역변수': 그 지역에서만 사용할 수 있는 변수 (예시- for문 안에서만 작동하고 for문 벗어나면 사라지는 애 )
 
 // [실습6]
 // 문제 1: prompt로 제품 정보 객체 만들기
@@ -117,26 +117,32 @@
 
 // 문제 7: 데이터 그룹화하기
 // 다음 team 배열을 department를 기준으로 그룹화하여, 아래 result와 같은 형태로 만드시오.
-const team = [  { name: '철수', department: '개발팀' },  { name: '영희', department: '기획팀' },  { name: '민수', department: '개발팀' },  { name: '지혜', department: '기획팀' }];
+// const team = [  { name: '철수', department: '개발팀' },  { name: '영희', department: '기획팀' },  { name: '민수', department: '개발팀' },  { name: '지혜', department: '기획팀' }];
 // 최종 결과 형태 (result)
 // { '개발팀': ['철수', '민수'], '기획팀': ['영희', '지혜'] }
-const newteam = [];
-for(let index = 0 ; index <= team.length-1 ; index++){
-   
-}
-console.log(newteam);
+               // const newteam = [];
+               // for(let index = 0 ; index <= team.length-1 ; index++){
+               //    if(team.indexOf(team[index].department) == -1 ){
+               //       newteam.push(team[index].department);
+               //    }
+               //    if(team[index].department)
+               //    team[index].nameㅠㅠ
+                  
+// 어 떻 게 해 !!
+               // }
+               // console.log(newteam);
 
 // 문제 8: 장바구니 총액 계산하기
 // 고객의 장바구니 정보를 담은 cart 배열과 상품 정보를 담은 productsInfo 배열이 있습니다.
 // cart 배열: 각 요소는 고객이 담은 상품의 id와 quantity(수량)를 가집니다.
 // productsInfo 배열: 각 요소는 상품의 고유 id와 price(가격)를 가집니다.
 // cart 배열을 기준으로, 장바구니에 담긴 모든 상품의 총 결제 금액을 계산하여 콘솔에 출력하세요.
-// const cart = [{ id: 1, quantity: 2 },{ id: 3, quantity: 1 }];
-// const productsInfo = [
-//   { id: 1, price: 1000 },
-//   { id: 2, price: 5000 }, // 장바구니에 없는 상품
-//   { id: 3, price: 2500 }
-// ];
+const cart = [{ id: 1, quantity: 2 },{ id: 3, quantity: 1 }];
+const productsInfo = [
+  { id: 1, price: 1000 },
+  { id: 2, price: 5000 }, // 장바구니에 없는 상품
+  { id: 3, price: 2500 }
+];
 
 // 문제 9: 투표 결과 집계하기
 // 다음 votes 배열은 투표 결과를 나타냅니다. 각 후보가 몇 표를 받았는지 집계하여, 후보의 이름이 키이고 득표수가 값인 객체를 만들어 콘솔에 출력하시오.
