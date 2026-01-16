7b2ff7c9da382550ac1f0dcc2a27ec754c28078e
leehyeon2021 <leehyeon2021@gmail.com>
2026-01-16 12:33:10 +0900

    260116 오전

---
+++ day016/view.js
@@ -0,0 +1,38 @@
+// [1] 쿼리스트링에서 특정한 게시물번호의 정보 조회
+getBoard(); // JS 실행될 때 최초1번 자동으로 함수 실행
+function getBoard(){
+    // 1. URL(웹주소) 상의 웹주소 가져오기
+    const url = new URLSearchParams(location.search);
+    // 2. 웹주소에서 선택된 게시물번호(쿼리스트링값) 가져오기
+    const selectNo = url.get('no');
+    // 3. localStorage에서 배열 가져오기 
+    let boardList = localStorage.getItem('boardList');
+    if(boardList == null){boardList = []}
+    else{boardList = JSON.parse(boardList)};
+    // 4. 선택된 게시물번호와 일치한 게시물 찾기 
+    for( let i = 0 ; i <= boardList.length-1 ; i++){
+        const obj = boardList[i];
+        if(obj.no == selectNo){
+            document.querySelector('#title').innerHTML = obj.title
+            document.querySelector('#content').innerHTML = obj.content;
+            return;
+        } // if end
+    } // for end
+} // func end
+
+
+// [2] 삭제함수
+function boardDelete(){
+    // 1. URL(웹주소)의 경로 가져오기
+    const url = new URLSearchParams(location.search);
+    // 2. 경로 상의 선택된 게시물번호(쿼리스트링) 가져오기
+    const selectNo = url.get('no');
+    // 3. localStorage에서 배열 가져오기
+    let boardList = localStorage.getItem('boardList');
+    
+
+}
+
+function boardUpdateView(){
+
+}
